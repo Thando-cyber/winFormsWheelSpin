b0ecaf9a7711efad724eb3b3842983fdc0d66f0e
Thandolwethu Sithole <thandosithole915@gmail.com>
2023-12-18 14:55:15 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace winFormsWheelSpin
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1=new PictureBox();
+            label1=new Label();
+            button1=new Button();
+            pictureBox2=new PictureBox();
+            label2=new Label();
+            label3=new Label();
+            pictureBox3=new PictureBox();
+            label4=new Label();
+            numericUpDown1=new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image=Properties.Resources.Spin_the_Wheel_Logo;
+            pictureBox1.Location=new Point(621, 51);
+            pictureBox1.Name="pictureBox1";
+            pictureBox1.Size=new Size(167, 159);
+            pictureBox1.SizeMode=PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex=0;
+            pictureBox1.TabStop=false;
+            // 
+            // label1
+            // 
+            label1.AutoSize=true;
+            label1.Location=new Point(632, 238);
+            label1.Name="label1";
+            label1.Size=new Size(146, 15);
+            label1.TabIndex=1;
+            label1.Text="Choose the Winning value";
+            // 
+            // button1
+            // 
+            button1.BackColor=Color.Gray;
+            button1.Location=new Point(651, 315);
+            button1.Name="button1";
+            button1.Size=new Size(108, 57);
+            button1.TabIndex=2;
+            button1.Text="Spin To Win";
+            button1.UseVisualStyleBackColor=false;
+            button1.Click+=button1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image=Properties.Resources.Wheel_Test5;
+            pictureBox2.Location=new Point(33, 51);
+            pictureBox2.Name="pictureBox2";
+            pictureBox2.Size=new Size(366, 367);
+            pictureBox2.SizeMode=PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex=3;
+            pictureBox2.TabStop=false;
+            // 
+            // label2
+            // 
+            label2.AutoSize=true;
+            label2.ForeColor=Color.Teal;
+            label2.Location=new Point(708, 393);
+            label2.Name="label2";
+            label2.Size=new Size(38, 15);
+            label2.TabIndex=4;
+            label2.Text="label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize=true;
+            label3.ForeColor=Color.Black;
+            label3.Location=new Point(600, 389);
+            label3.Name="label3";
+            label3.Size=new Size(28, 15);
+            label3.TabIndex=5;
+            label3.Text="Win";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image=Properties.Resources.arrow;
+            pictureBox3.Location=new Point(246, 12);
+            pictureBox3.Name="pictureBox3";
+            pictureBox3.Size=new Size(40, 42);
+            pictureBox3.SizeMode=PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex=6;
+            pictureBox3.TabStop=false;
+            // 
+            // label4
+            // 
+            label4.AutoSize=true;
+            label4.ForeColor=Color.Chartreuse;
+            label4.Location=new Point(425, 389);
+            label4.Name="label4";
+            label4.Size=new Size(169, 15);
+            label4.TabIndex=7;
+            label4.Text="Congtratulations yu have won ";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Increment=new decimal(new int[] { 10000, 0, 0, 0 });
+            numericUpDown1.Location=new Point(639, 272);
+            numericUpDown1.Maximum=new decimal(new int[] { 200000, 0, 0, 0 });
+            numericUpDown1.Name="numericUpDown1";
+            numericUpDown1.Size=new Size(120, 23);
+            numericUpDown1.TabIndex=9;
+            numericUpDown1.Value=new decimal(new int[] { 10000, 0, 0, 0 });
+            numericUpDown1.ValueChanged+=numericUpDown1_ValueChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions=new SizeF(7F, 15F);
+            AutoScaleMode=AutoScaleMode.Font;
+            BackColor=Color.White;
+            ClientSize=new Size(800, 450);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label4);
+            Controls.Add(pictureBox3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            ForeColor=Color.Gold;
+            Name="Form1";
+            Text="Form1";
+            Load+=Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private Label label2;
+        private Label label3;
+        private PictureBox pictureBox3;
+        private Label label4;
+        private NumericUpDown numericUpDown1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System.Media;
+using Timer = System.Windows.Forms.Timer;
+
+namespace winFormsWheelSpin
+{
+    public partial class Form1 : Form
+    {
+        public int val;
+        bool wheelIsMoved;
+        float wheelTimes;
+        Timer wheelTimer;
+        SpinOWheel spin;
+        public Form1()
+        {
+            InitializeComponent();
+            wheelTimer = new Timer();
+            wheelTimer.Interval = 30;
+            wheelTimer.Tick += wheelTimer_Tick;
+            spin = new SpinOWheel();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public class SpinOWheel
+        {
+            public Bitmap obrazek;
+            public Bitmap tempObrazek;
+            public float kat;
+            public int[] wartosciStanu;
+            public int stan;
+            public SpinOWheel()
+            {
+                tempObrazek = new Bitmap(Properties.Resources.Wheel_Test5);
+                obrazek = new Bitmap(Properties.Resources.Wheel_Test5);
+                wartosciStanu = new int[] { 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 09, 08, 07, 06, 05, 04, 03, 02, 01 };
+                kat = 0.0f;
+            }
+        }
+        public static Bitmap RotateImage(Image image, float angle)
+        {
+            return RotateImage(image, new PointF((float)image.Width / 2, (float)image.Height / 2), angle);
+        }
+        public static Bitmap RotateImage(Image image, PointF offset, float angle)
+        {
+            if (image == null)
+                throw new ArgumentNullException("image");
+
+
+            Bitmap rotatedBmp = new Bitmap(image.Width, image.Height);
+            rotatedBmp.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+            Graphics g = Graphics.FromImage(rotatedBmp);
+            g.TranslateTransform(offset.X, offset.Y);
+            g.RotateTransform(angle);
+            g.TranslateTransform(-offset.X, -offset.Y);
+            g.DrawImage(image, new PointF(0, 0));
+
+            return rotatedBmp;
+        }
+        private void RotateImage(PictureBox pb, Image img, float angle)
+        {
+            if (img == null || pb.Image == null)
+                return;
+
+            Image oldImage = pb.Image;
+            pb.Image = RotateImage(img, angle);
+            if (oldImage != null)
+            {
+                oldImage.Dispose();
+            }
+
+        }
+        private void wheelTimer_Tick(object sender, EventArgs e)
+        {
+
+            if (wheelIsMoved && wheelTimes > 0)
+            {
+                spin.kat += wheelTimes / 10;
+                spin.kat = spin.kat % 360;
+                RotateImage(pictureBox2, spin.obrazek, spin.kat);
+                wheelTimes--;
+                playSimpleSound();
+            }
+            spin.stan = Convert.ToInt32(Math.Ceiling(spin.kat / 18));
+            //string value = Convert.ToString(spin.wartosciStanu[spin.stan]);
+
+            if (spin.stan == 0)
+            {
+                spin.stan = 0;
+            }
+            else
+            {
+                spin.stan -= 1;
+            }
+
+            label2.Text = Convert.ToString(spin.wartosciStanu[spin.stan]);
+            int win = Int32.Parse(Convert.ToString(spin.wartosciStanu[spin.stan]));
+            if (win==20)
+            {
+                val = 10000;
+            }
+            else
+            {
+                val = (win*10000)+10000;
+            }
+            label3.Text = Convert.ToString(val);
+            //MessageBox.Show("Congratulations ! You've Won R" + val);
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wheelIsMoved = true;
+            Random rand = new Random();
+            wheelTimes = rand.Next(150, 180);    //random số vòng quay
+            //string winVal = comboBox1.Text.Remove(0);
+            //int userVal= Int32.Parse(winVal);
+            //int stopVal = 0;
+
+            string value = Convert.ToString(spin.wartosciStanu[spin.stan]);
+            int win = Int32.Parse(value);
+
+            wheelTimer.Start();
+
+
+        }
+        private void playSimpleSound()
+        {
+            SoundPlayer ticksound = new SoundPlayer("C:\\Users\\Makro\\Documents\\winFormsWheelSpin\\Resources\\Tick.wav");
+            ticksound.Play();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
